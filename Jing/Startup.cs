60c74a672b5423732756cb7309c18454ba47b8f7
jsun24 <jsun24@CDM-CD400LAB30>
2016-10-26 12:14:43 -0500

    Add project files.

---
+++ Jing/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Jing.Startup))]
+namespace Jing
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
