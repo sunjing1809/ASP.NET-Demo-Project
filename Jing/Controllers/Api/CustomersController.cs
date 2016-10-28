dea7f15088bea502c81cfcbf88d69e1fec946bb0
sunjing1809 <sunjing1809@gmail.com>
2016-10-28 13:15:44 -0500

    Add Client and Server Side Validation for New/Edit Customer Form and New/Edit Movie Form

---
+++ Jing/Controllers/Api/CustomersController.cs
@@ -0,0 +1,82 @@
+﻿using Jing.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Jing.Controllers.Api
+{
+   
+    public class CustomersController : ApiController
+    {
+        private ApplicationDbContext  _context;
+        public CustomersController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        
+        //GET /api/customers
+        public IEnumerable<Customer> GetCustomers()
+        {
+            return _context.Customers.ToList();
+        }
+
+        //GET /api/customers/1
+        public Customer GetCustomers(int id)
+        {
+            var customer = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customer == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            return customer;
+        }
+
+        //POST /api/customers
+        [HttpPost]
+        public Customer CreateCustomer(Customer customer)
+        {
+            if(!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            _context.Customers.Add(customer);
+            _context.SaveChanges();
+
+            return customer;
+
+        }
+
+        //Put  /api/customers/1
+        [HttpPut]
+        public void UpdateCustomer(int id, Customer customer)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            
+            if(customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            customerInDb.Name = customer.Name;
+            customerInDb.Birthday = customer.Birthday;
+            customerInDb.IsSubscribedToNewsletter = customer.IsSubscribedToNewsletter;
+            customerInDb.MembershipTypeId = customer.MembershipTypeId;
+
+            _context.SaveChanges();
+        }
+
+        //Delete /api/customers/1
+        [HttpDelete]
+        public void DeleteCustomer(int id)
+        {
+            var customerInDb = _context.Customers.SingleOrDefault(c => c.Id == id);
+            if (customerInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            _context.Customers.Remove(customerInDb);
+
+            _context.SaveChanges();
+
+        }
+
+    }
+}
--- Jing/Models/Customer.cs
+++ Jing/Models/Customer.cs
@@ -22,6 +22,7 @@ namespace Jing.Models
         public byte MembershipTypeId { get; set; }
 
         [Display(Name = "Date of Birth")]
+        [Min18YearsValidation]
         public DateTime? Birthday { get; set; }
 
     }
